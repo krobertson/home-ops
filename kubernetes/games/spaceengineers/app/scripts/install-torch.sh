288d42526d28e742586c7e80544ffa7e2afdd44b
Ken Robertson <ken@invalidlogic.com>
2026-02-22 18:02:29 -0800

    Updates to handle restoring a backup, add backups

--- kubernetes/games/spaceengineers/app/scripts/install-torch.sh
+++ kubernetes/games/spaceengineers/app/scripts/install-torch.sh
@@ -2,18 +2,24 @@
 
 set -e
 
+## If env has a URL to a save to restore, do it if no existing configs are present.
+if [ ! -z "${RESTORE_URL}" ] && [ ! -e Torch.cfg ] && [ ! -e Instance/SpaceEngineers-Dedicated.cfg ]; then
+  echo ''
+  echo 'Restoring existing configuration...'
+  echo ''
+  curl -sSL -o /tmp/restore.zip ${RESTORE_URL}
+  unzip -o /tmp/restore.zip -d /game/
+  rm /tmp/restore.zip
+fi
+
 ## Download Torch and unpack
 if [ ! -e Torch.Server.exe ]; then
-  (
-    echo ''
-    echo 'Installing Space Engineers Torch...'
-    echo ''
-    cd /tmp
-    curl -sSL -o torch-server.zip https://build.torchapi.com/job/Torch/job/master/lastSuccessfulBuild/artifact/bin/torch-server.zip
-    unzip -o torch-server.zip -d /game/
-    rm torch-server.zip
-  )
-
+  echo ''
+  echo 'Installing Space Engineers Torch...'
+  echo ''
+  curl -sSL -o /tmp/torch-server.zip https://build.torchapi.com/job/Torch/job/master/lastSuccessfulBuild/artifact/bin/torch-server.zip
+  unzip -o /tmp/torch-server.zip -d /game/
+  rm /tmp/torch-server.zip
 fi
 
 ## Copy initial configuration files
